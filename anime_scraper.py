48c8c794182d46ab3e696b35263f6dc46371af91
Dani-r-36 <danishanr0@gmail.com>
2023-06-09 12:19:31 +0100

    genre search added

--- anime_scraper.py
+++ anime_scraper.py
@@ -4,11 +4,13 @@ import psycopg2.extras
 import re
 from bs4 import BeautifulSoup
 from rich.console import Console
+from rapidfuzz import fuzz
 
 console = Console()
 
 ANIME_SEARCH = "https://myanimelist.net/anime.php?cat=anime&q="
 ANIME_HOME = "https://myanimelist.net/anime.php"
+ANIME_DEFAULT = "https://myanimelist.net"
 
 def get_html(url):
     """opens bbc website for extraction"""
@@ -33,16 +35,19 @@ def get_anime_similar(soup):
         url = anime.get('href')
         if url not in animes_url:
             animes_url.append(anime.get('href'))
-    anime_url = check_request(animes_title, animes_url)
+    anime_url = check_request(animes_title, animes_url, "anime")
     return anime_url
 
-def check_request(animes_title, animes_url):
-    console.print(animes_title)
-    while 1:
-        anime_request = input("Enter anime from list\n")
-        if anime_request in animes_title:
-            break
-    return animes_url[animes_title.index(anime_request)]
+def check_request(animes, animes_url, type_request):
+    console.print(animes)
+    found = False
+    while found == False:
+        anime_request = input(f"Enter {type_request} from list\n")
+        for anime in animes:
+            if int(fuzz.partial_ratio(anime_request, anime)) > 85:
+                found = True
+                break
+    return animes_url[animes.index(anime)]
 
 def extract_anime_info(soup):
     details = soup.find("div", {"class": "stats-block po-r clearfix"})
@@ -70,8 +75,20 @@ def get_anime_genre(soup):
             genre_split = [s.strip() + ')' for s in re.split(r'\)(?=[A-Z])', genre_content.text) if s.strip()]
             anime_genre_split.extend(genre_split)
             anime_genre_link.extend(href_link)
-    return anime_genre_link, anime_genre_split
+    anime_url = check_request(anime_genre_split, anime_genre_link, "genre")
+    return anime_url
 
+def extract_anime_genre_info(animes_soup):
+    anime_list = []
+    anime_urls = []
+    genre_animes = animes_soup.select("div.js-anime-category-producer.seasonal-anime.js-seasonal-anime.js-anime-type-all.js-anime-type-1")
+    for anime in genre_animes:
+        anime_details_better = anime.select("h2.h2_anime_title")
+        url = anime_details_better[0].find("a")
+        anime_list.append(anime_details_better[0].text)
+        anime_urls.append(url["href"])
+    anime_url = check_request(anime_list, anime_urls, "anime")
+    return anime_url
 
 if __name__ == "__main__":
     # requested_anime = input("Enter anime to search\n")
@@ -81,6 +98,9 @@ if __name__ == "__main__":
     # anime_soup = parse_anime(anime_url)
     # extract_anime_info(anime_soup)
 
-    requested_genre = input("Enter genre/theme of anime\n")
     genre_soup = parse_anime(ANIME_HOME)
-    get_anime_genre(genre_soup)
+    anime_genres_url = get_anime_genre(genre_soup)
+    anime_genre_soup = parse_anime(f"{ANIME_DEFAULT}{anime_genres_url}")
+    anime_genre_url = extract_anime_genre_info(anime_genre_soup)
+    anime_genre_soup = parse_anime(anime_genre_url)
+    extract_anime_info(anime_genre_soup)
